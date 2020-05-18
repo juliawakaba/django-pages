c1424e8954952752741ad60f37a100da0d184394
jmugure95 <jmugure95@gmail.com>
2020-05-18 16:57:28 +0300

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "pages"
+
 urlpatterns = [
-    path('', views.homepage_view, name="home")
+    path('', views.homepage_view, name='home'),
+    path('about/', views.about_view, name='abouts'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage_view(request):
-	return HttpResponse("Hello World!")
+	return render(request, 'pages/index.html')
+
+def about_view(request):
+	return render(request, 'pages/about.html')
